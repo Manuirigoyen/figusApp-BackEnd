67ff89940e8139958f2f51da635d854f01140321
Martín Lorenzi <alexmartin9c@gmail.com>
2026-06-26 19:50:10 -0300

    Update auth.service.ts

--- src/module/auth/auth.service.ts
+++ src/module/auth/auth.service.ts
@@ -1,6 +1,5 @@
 import { Controller, Post, Body, Res, HttpCode, HttpStatus } from '@nestjs/common';
 import { Response } from 'express'; 
-import { AuthService } from './auth.service';
 import { LoginUserDto } from '../users/dto/login-user.dto';
 
 @Controller('auth')
